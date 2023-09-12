2e6a1513b48bf79f10aebfbb5b231a5bd183f8cb
peponi <peponi_paradise@naver.com>
2023-09-13 07:48:54 +0900

    Volume, dry volume, weight 추가.

--- Peponi.Tests/Peponi.Math.Tests/UnitConversion/UnitConvert.cs
+++ Peponi.Tests/Peponi.Math.Tests/UnitConversion/UnitConvert.cs
@@ -7,37 +7,58 @@ namespace Peponi.Math.Tests.UnitConversion;
 public class UnitConversion
 {
     [TestMethod]
-    public void AreaTest()
+    public void Area()
     {
         var result = UnitConvert.Convert(451.12031d, AreaUnit.Perch, AreaUnit.MileUS);
         Assert.AreEqual(0.004405, result.Round(6));
     }
 
     [TestMethod]
-    public void LengthTest()
+    public void DryVolume()
+    {
+        var result = UnitConvert.Convert(451.12031d, DryVolumeUnit.BushelUS, DryVolumeUnit.PeckUK);
+        Assert.AreEqual(1748.432198, result.Round(6));
+    }
+
+    [TestMethod]
+    public void Length()
     {
         var result = UnitConvert.Convert(451.12031d, LengthUnit.Furlong, LengthUnit.MileUSStatute);
         Assert.AreEqual(56.389926, result.Round(6));
     }
 
     [TestMethod]
-    public void PressureTest()
+    public void Prefix()
+    {
+        var result = UnitConvert.Convert(500d, PrefixUnit.Peta, PrefixUnit.Deci);
+        Assert.AreEqual(5E+18, result.Round(6));
+    }
+
+    [TestMethod]
+    public void Pressure()
     {
         var result = UnitConvert.Convert(500d, PressureUnit.kipfPerSquareInch, PressureUnit.atm);
         Assert.AreEqual(34022.981955, result.Round(6));
     }
 
     [TestMethod]
-    public void TemperatureTest()
+    public void Temperature()
     {
         var result = UnitConvert.Convert(500d, TemperatureUnit.Reaumur, TemperatureUnit.Kelvin);
         Assert.AreEqual(898.15, result.Round(6));
     }
 
     [TestMethod]
-    public void PrefixTest()
+    public void Volume()
     {
-        var result = UnitConvert.Convert(500d, PrefixUnit.Peta, PrefixUnit.Deci);
-        Assert.AreEqual(5E+18, result.Round(6));
+        var result = UnitConvert.Convert(451.12031d, VolumeUnit.Cord, VolumeUnit.HundredCubicFoot);
+        Assert.AreEqual(577.433997, result.Round(6));
+    }
+
+    [TestMethod]
+    public void Weight()
+    {
+        var result = UnitConvert.Convert(451.12031d, WeightUnit.DekaGram, WeightUnit.StoneUK);
+        Assert.AreEqual(0.710393, result.Round(6));
     }
 }
--- Peponi/Peponi.Math/Extensions/UnitConvert.cs
+++ Peponi/Peponi.Math/Extensions/UnitConvert.cs
@@ -9,9 +9,19 @@ public static class UnitConvertExtensions
         return Area.ConvertTo(value, convertFrom, convertTo);
     }
 
+    public static T Convert<T>(this T value, DryVolumeUnit convertFrom, DryVolumeUnit convertTo) where T : struct
+    {
+        return DryVolume.ConvertTo(value, convertFrom, convertTo);
+    }
+
     public static T Convert<T>(this T value, LengthUnit convertFrom, LengthUnit convertTo) where T : struct
     {
         return Length.ConvertTo(value, convertFrom, convertTo);
+    }
+
+    public static T Convert<T>(this T value, PrefixUnit convertFrom, PrefixUnit convertTo) where T : struct
+    {
+        return Prefix.ConvertTo(value, convertFrom, convertTo);
     }
 
     public static T Convert<T>(this T value, PressureUnit convertFrom, PressureUnit convertTo) where T : struct
@@ -24,8 +34,13 @@ public static class UnitConvertExtensions
         return Temperature.ConvertTo(value, convertFrom, convertTo);
     }
 
-    public static T Convert<T>(this T value, PrefixUnit convertFrom, PrefixUnit convertTo) where T : struct
+    public static T Convert<T>(this T value, VolumeUnit convertFrom, VolumeUnit convertTo) where T : struct
     {
-        return Prefix.ConvertTo(value, convertFrom, convertTo);
+        return Volume.ConvertTo(value, convertFrom, convertTo);
+    }
+
+    public static T Convert<T>(this T value, WeightUnit convertFrom, WeightUnit convertTo) where T : struct
+    {
+        return Weight.ConvertTo(value, convertFrom, convertTo);
     }
 }
