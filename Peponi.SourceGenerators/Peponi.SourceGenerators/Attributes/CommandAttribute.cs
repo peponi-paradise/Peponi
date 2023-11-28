2e026a687d6eaf180758a7c999d5756b2e38c228
peponi <peponi_paradise@naver.com>
2023-11-29 07:37:33 +0900

    CommandAttribute 주석 작성 시작

--- Peponi.SourceGenerators/Peponi.SourceGenerators/Attributes/CommandAttribute.cs
+++ Peponi.SourceGenerators/Peponi.SourceGenerators/Attributes/CommandAttribute.cs
@@ -1,8 +1,84 @@
-﻿namespace Peponi.SourceGenerators;
+﻿using System.Windows.Input;
 
+namespace Peponi.SourceGenerators;
+
+/// <summary>
+/// Use this attribute for generating <see cref="ICommand"/> members<br/>
+/// Partial type declaration is required for using this attribute<br/>
+/// Generated method's name has "Command" suffix
+/// <para>
+/// Input and generated code looks like followings:
+/// <code>
+/// // Input
+/// public partial class CodeTest
+/// {
+///    [Command]
+///    private void Test()
+///    {
+///        return;
+///    }
+/// }
+/// </code>
+/// <code>
+/// // Generated
+/// public partial class CodeTest
+/// {
+///     private CommandBase? _testCommand;
+///
+///     public ICommandBase TestCommand => _testCommand ??= new CommandBase(Test);
+/// }
+/// </code>
+/// <code>
+/// // Input
+/// public partial class CodeTest
+/// {
+///     [Command]
+///     private async Task Test()
+///     {
+///         return;
+///     }
+/// }
+/// </code>
+/// <code>
+/// // Generated
+/// public partial class CodeTest
+/// {
+///     private CommandBase? _testCommand;
+///
+///     public ICommandBase TestCommand => _testCommand ??= new CommandBase(async () => { await Test(); });</code>
+/// }
+/// </para>
+/// </summary>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
 public class CommandAttribute : Attribute
 {
+    /// <summary>
+    /// Sets the name of command
+    /// Basically, generated backing member's name is target method's name with "Command" suffix
+    /// <para>
+    /// Input and generated code looks like followings:
+    /// <code>
+    /// // Input
+    /// public partial class CodeTest
+    /// {
+    ///     [Command(CustomName = "MyCommand")]
+    ///     private void Test()
+    ///     {
+    ///         return;
+    ///     }
+    /// }
+    /// </code>
+    /// <code>
+    /// // Generated
+    /// public partial class CodeTest
+    /// {
+    ///     private CommandBase? _testCommand;
+    ///
+    ///     public ICommandBase MyCommand => _testCommand ??= new CommandBase(Test);</code>
+    /// }
+    /// </para>
+    /// </summary>
     public string? CustomName { get; set; }
+
     public string? CanExecute { get; set; }
 }
