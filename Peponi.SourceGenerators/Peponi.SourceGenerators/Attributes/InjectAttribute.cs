b1ba138735696bbbe5e8b935b7c2e5eef7199a6a
peponi <peponi_paradise@naver.com>
2023-12-07 07:31:58 +0900

    InjectAttribute 주석 작성

--- Peponi.SourceGenerators/Peponi.SourceGenerators/Attributes/InjectAttribute.cs
+++ Peponi.SourceGenerators/Peponi.SourceGenerators/Attributes/InjectAttribute.cs
@@ -64,7 +64,7 @@ public class InjectAttribute : Attribute
     /// }
     /// </code>
     /// <code>
-    /// // Output
+    /// // Generated
     /// public partial class CodeTest
     /// {
     ///     public global::GeneratorTest.BaseClass BaseClass;
@@ -110,8 +110,126 @@ public class InjectAttribute : Attribute
     /// </summary>
     public InjectionType InjectionMode { get; set; } = InjectionType.Dependency | InjectionType.Model;
 
+    /// <summary>
+    /// Sets the name of injecting target<br/>
+    /// Basically, generated member's name is same as injecting target
+    /// <para>
+    /// <code>
+    /// // Input
+    /// public class BaseClass
+    /// {
+    ///     public int TestInt = 0;
+    ///     public bool TestBool = false;
+    /// }
+    ///
+    /// [Inject(typeof(BaseClass), InjectionType.Dependency, CustomName = "InjectClass")]
+    /// public partial class CodeTest
+    /// {
+    /// }
+    /// </code>
+    /// <code>
+    /// // Generated
+    /// public partial class CodeTest
+    /// {
+    ///     public global::GeneratorTest.BaseClass InjectClass;
+    ///
+    ///     public CodeTest(global::GeneratorTest.BaseClass InjectClass)
+    ///     {
+    ///         this.InjectClass = InjectClass;
+    ///     }
+    /// }
+    /// </code>
+    /// </para>
+    /// </summary>
     public string? CustomName { get; set; }
+
+    /// <summary>
+    /// Sets the modifier of injecting target<br/>
+    /// <para>
+    /// <code>
+    /// // Input
+    /// public class BaseClass
+    /// {
+    ///     public int TestInt = 0;
+    ///     public bool TestBool = false;
+    /// }
+    ///
+    /// [Inject(typeof(BaseClass), InjectionType.Dependency, TypeModifier = Modifier.Protected)]
+    /// public partial class CodeTest
+    /// {
+    /// }
+    /// </code>
+    /// <code>
+    /// // Generated
+    /// public partial class CodeTest
+    /// {
+    ///     protected global::GeneratorTest.BaseClass _baseClass;
+    ///
+    ///     public CodeTest(global::GeneratorTest.BaseClass _baseClass)
+    ///     {
+    ///         this._baseClass = _baseClass;
+    ///     }
+    /// }
+    /// </code>
+    /// </para>
+    /// </summary>
     public Modifier TypeModifier { get; set; } = Modifier.Public;
+
+    /// <summary>
+    /// Sets the notify mode of injected target's member<br/>
+    /// This is valid for <see cref="InjectionType.Model"/>
+    /// <para>
+    /// <code>
+    /// // Input
+    /// public class BaseClass
+    /// {
+    ///     public int TestInt = 0;
+    ///     public bool TestBool = false;
+    /// }
+    ///
+    /// [Inject(typeof(BaseClass), InjectionType.Model, PropertyNotifyMode = NotifyType.None)]
+    /// public partial class CodeTest
+    /// {
+    /// }
+    /// </code>
+    /// <code>
+    /// // Generated
+    /// public partial class CodeTest
+    /// {
+    ///     public global::GeneratorTest.BaseClass BaseClass;
+    ///
+    ///     public int TestInt
+    ///     {
+    ///         get => BaseClass.TestInt;
+    ///         set
+    ///         {
+    ///             if(BaseClass.TestInt != value)
+    ///             {
+    ///                 BaseClass.TestInt = value;
+    ///                 OnTestIntChanged();
+    ///             }
+    ///         }
+    ///     }
+    ///
+    ///     public bool TestBool
+    ///     {
+    ///         get => BaseClass.TestBool;
+    ///         set
+    ///         {
+    ///             if(BaseClass.TestBool != value)
+    ///             {
+    ///                 BaseClass.TestBool = value;
+    ///                 OnTestBoolChanged();
+    ///             }
+    ///         }
+    ///     }
+    ///
+    ///     partial void OnTestIntChanged();
+    ///     partial void OnTestBoolChanged();
+    /// }
+    /// </code>
+    /// </para>
+    /// </summary>
     public NotifyType PropertyNotifyMode { get; set; } = NotifyType.Notify;
 
     public InjectAttribute(Type type, InjectionType injectionMode)
