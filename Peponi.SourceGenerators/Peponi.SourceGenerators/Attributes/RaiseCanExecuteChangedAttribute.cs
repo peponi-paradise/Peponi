fec99388807631d7e65ff4c3ce3daf8ecf656e57
peponi <peponi_paradise@naver.com>
2023-12-08 11:45:56 +0900

    RaiseCanExecuteChangedAttribute, RaisePropertyChangedAttribute 주석 작성

--- Peponi.SourceGenerators/Peponi.SourceGenerators/Attributes/RaiseCanExecuteChangedAttribute.cs
+++ Peponi.SourceGenerators/Peponi.SourceGenerators/Attributes/RaiseCanExecuteChangedAttribute.cs
@@ -1,8 +1,44 @@
-﻿namespace Peponi.SourceGenerators;
+﻿using Peponi.SourceGenerators.Commands;
 
+namespace Peponi.SourceGenerators;
+
+/// <summary>
+/// This is an attribute for raising whether certain command could execute or not<br/>
+/// Generated property will call <see cref="ICommandBase.RaiseCanExecuteChanged"/> at setter
+/// <para>
+/// <code>
+/// // Input
+/// [Property]
+/// [RaiseCanExecuteChanged("TestCommand")]
+/// private bool _testBool = false;
+/// </code>
+/// <code>
+/// // Generated
+/// public bool TestBool
+/// {
+///     get => _testBool;
+///     set
+///     {
+///         if(_testBool != value)
+///         {
+///             _testBool = value;
+///             OnPropertyChanged(nameof(TestBool));
+///             OnTestBoolChanged();
+///             TestCommand.RaiseCanExecuteChanged();
+///         }
+///     }
+/// }
+///
+/// partial void OnTestBoolChanged();
+/// </code>
+/// </para>
+/// </summary>
 [AttributeUsage(AttributeTargets.Field, AllowMultiple = true, Inherited = false)]
 public class RaiseCanExecuteChangedAttribute : Attribute
 {
+    /// <summary>
+    /// Command's name that will be raised
+    /// </summary>
     public string CommandName { get; set; }
 
     public RaiseCanExecuteChangedAttribute(string commandName)
--- Peponi.SourceGenerators/Peponi.SourceGenerators/Attributes/RaisePropertyChangedAttribute.cs
+++ Peponi.SourceGenerators/Peponi.SourceGenerators/Attributes/RaisePropertyChangedAttribute.cs
@@ -1,8 +1,44 @@
-﻿namespace Peponi.SourceGenerators;
+﻿using System.ComponentModel;
 
+namespace Peponi.SourceGenerators;
+
+/// <summary>
+/// This is an attribute for raising <see cref="INotifyPropertyChanged.PropertyChanged"/> for other property<br/>
+/// Generated property will call <see cref="INotifyPropertyChanged.PropertyChanged"/> at setter
+/// <para>
+/// <code>
+/// // Input
+/// [Property]
+/// [RaisePropertyChanged("TestParam")]
+/// private bool _testBool = false;
+/// </code>
+/// <code>
+/// // Generated
+/// public bool TestBool
+/// {
+///     get => _testBool;
+///     set
+///     {
+///         if(_testBool != value)
+///         {
+///             _testBool = value;
+///             OnPropertyChanged(nameof(TestBool));
+///             OnTestBoolChanged();
+///             OnPropertyChanged(nameof(TestParam));
+///         }
+///     }
+/// }
+///
+/// partial void OnTestBoolChanged();
+/// </code>
+/// </para>
+/// </summary>
 [AttributeUsage(AttributeTargets.Field, AllowMultiple = true, Inherited = false)]
 public class RaisePropertyChangedAttribute : Attribute
 {
+    /// <summary>
+    /// Property's name that will be raised
+    /// </summary>
     public string PropertyName { get; set; }
 
     public RaisePropertyChangedAttribute(string propertyName)
