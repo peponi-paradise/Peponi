7124693b8688cc8e9619d93c15419ab0d1c6798e
peponi <peponi_paradise@naver.com>
2023-12-15 11:59:19 +0900

    Log 개선작업 진행중

--- Peponi.Logger/Peponi.Logger/Log.Define.cs
+++ Peponi.Logger/Peponi.Logger/Log.Define.cs
@@ -1,45 +1,249 @@
-﻿namespace Peponi.Logger;
+﻿using Peponi.Utility.Helpers;
 
-public enum LogWriteOption
+namespace Peponi.Logger;
+
+/// <summary>
+/// All log folders are divided by log folder tree
+/// </summary>
+public enum LogDirectoryTree
 {
-    /// <summary>
-    /// All logs are in one file
-    /// </summary>
-    OneFile = 1,
+    None = 0,
+
+    LoggerName = 1,
 
     /// <summary>
-    /// Separate files by log type
+    /// DateTime format "ss"
     /// </summary>
-    SeperateFile = 2,
+    DateTime_Second = 10,
 
     /// <summary>
-    /// Separate folders by log type
+    /// DateTime format "mm"
     /// </summary>
-    SeperateFolder = 3,
+    DateTime_Minute = 11,
+
+    /// <summary>
+    /// DateTime format "HH"
+    /// </summary>
+    DateTime_Hour = 12,
+
+    /// <summary>
+    /// DateTime format "dd"
+    /// </summary>
+    DateTime_Day = 13,
+
+    /// <summary>
+    /// DateTime format "MM"
+    /// </summary>
+    DateTime_Month = 14,
+
+    /// <summary>
+    /// DateTime format "yyyyy"
+    /// </summary>
+    DateTime_Year = 15,
 }
 
-public class LogOption
+public enum LogFileCreatingRule
+{
+    LoggerName = 0,
+    CustomName = 1,
+
+    /// <summary>
+    /// DateTime format "ss"
+    /// </summary>
+    DateTime_Second = 10,
+
+    /// <summary>
+    /// DateTime format "mm"
+    /// </summary>
+    DateTime_Minute = 11,
+
+    /// <summary>
+    /// DateTime format "HH"
+    /// </summary>
+    DateTime_Hour = 12,
+
+    /// <summary>
+    /// DateTime format "dd"
+    /// </summary>
+    DateTime_Day = 13,
+
+    /// <summary>
+    /// DateTime format "MM"
+    /// </summary>
+    DateTime_Month = 14,
+
+    /// <summary>
+    /// DateTime format "yyyyy"
+    /// </summary>
+    DateTime_Year = 15,
+
+    /// <summary>
+    /// Insert ' . '
+    /// </summary>
+    Dot = 100,
+
+    /// <summary>
+    /// Insert empty space '  '
+    /// </summary>
+    Space = 102,
+
+    /// <summary>
+    /// Insert ' _ '
+    /// </summary>
+    Underbar = 103,
+
+    /// <summary>
+    /// Insert ' - '
+    /// </summary>
+    Dash = 104,
+}
+
+/// <summary>
+/// All logs are written by given pattern
+/// </summary>
+public enum LogMessagePattern
+{
+    None = 0,
+
+    LoggerName = 1,
+
+    /// <summary>
+    /// [yyyy.MM.dd HH:mm:ss.fff]
+    /// </summary>
+    DateTime = 2,
+
+    Message = 3,
+
+    /// <summary>
+    /// Environment.NewLine
+    /// </summary>
+    NewLine = 1000,
+}
+
+public class LogOption : IEquatable<LogOption?>
 {
     /// <summary>
-    /// Any enum type user defined
+    /// Logging base key name
     /// </summary>
-    public Enum? LogType;
+    public string LoggerName;
 
-    public LogWriteOption LogWriteOption = LogWriteOption.SeperateFolder;
+    public LogDirectoryOption DirectoryOption = new();
+    public LogFileOption FileOption = new();
+    public LogMessageOption MessageOption = new();
 
+    public LogOption(string loggerName, LogDirectoryOption directoryOption, LogFileOption fileOption)
+    {
+        LoggerName = loggerName;
+        DirectoryOption = directoryOption;
+        FileOption = fileOption;
+    }
+
+    public LogOption(string loggerName, LogDirectoryOption directoryOption, LogFileOption fileOption, LogMessageOption messageOption)
+    {
+        LoggerName = loggerName;
+        DirectoryOption = directoryOption;
+        FileOption = fileOption;
+        MessageOption = messageOption;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as LogOption);
+    }
+
+    public bool Equals(LogOption? other)
+    {
+        return other is not null &&
+               LoggerName == other.LoggerName &&
+               EqualityComparer<LogDirectoryOption>.Default.Equals(DirectoryOption, other.DirectoryOption) &&
+               EqualityComparer<LogFileOption>.Default.Equals(FileOption, other.FileOption) &&
+               EqualityComparer<LogMessageOption>.Default.Equals(MessageOption, other.MessageOption);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(LoggerName, DirectoryOption, FileOption, MessageOption);
+    }
+
+    public static bool operator ==(LogOption? left, LogOption? right)
+    {
+        return EqualityComparer<LogOption>.Default.Equals(left, right);
+    }
+
+    public static bool operator !=(LogOption? left, LogOption? right)
+    {
+        return !(left == right);
+    }
+}
+
+public class LogDirectoryOption : IEquatable<LogDirectoryOption?>
+{
     /// <summary>
     /// Root path of log
     /// </summary>
     public string RootPath = $@"{Environment.CurrentDirectory}\Log\";
 
-    /// <summary>
-    /// Saving time unit of log.<br/>
-    /// Equal to DateTime.Now.ToString() format<br/><br/>
-    /// ex:<br/>
-    /// "yyyy-MM-dd"
-    /// </summary>
-    public string LogFilePattern = "yyyy-MM-dd";
+    public List<LogDirectoryTree> DirectoryTree = new();
 
+    internal string CreateFolderTree(string loggerName, DateTime logTime)
+    {
+        string rtnPath = (string)RootPath.Clone();
+        foreach (var tree in DirectoryTree)
+        {
+            if (tree != LogDirectoryTree.None)
+            {
+                rtnPath = Path.Combine(rtnPath, GetPath(tree));
+                DirectoryHelper.CreateDirectory(rtnPath);
+            }
+        }
+        return rtnPath;
+
+        string GetPath(LogDirectoryTree treeItem)
+        {
+            return treeItem switch
+            {
+                LogDirectoryTree.LoggerName => @$"{loggerName}\",
+                LogDirectoryTree.DateTime_Second => $@"{logTime:ss}\",
+                LogDirectoryTree.DateTime_Minute => $@"{logTime:mm}\",
+                LogDirectoryTree.DateTime_Hour => $@"{logTime:HH}\",
+                LogDirectoryTree.DateTime_Day => $@"{logTime:dd}\",
+                LogDirectoryTree.DateTime_Month => $@"{logTime:MM}\",
+                LogDirectoryTree.DateTime_Year => $@"{logTime:yyyy}\",
+                _ => throw new ArgumentException($"Not supported tree item : {treeItem}")
+            };
+        }
+    }
+
+    public static bool operator ==(LogDirectoryOption? left, LogDirectoryOption? right)
+    {
+        return EqualityComparer<LogDirectoryOption>.Default.Equals(left, right);
+    }
+
+    public static bool operator !=(LogDirectoryOption? left, LogDirectoryOption? right)
+    {
+        return !(left == right);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as LogDirectoryOption);
+    }
+
+    public bool Equals(LogDirectoryOption? other)
+    {
+        return other is not null &&
+               RootPath == other.RootPath &&
+               EqualityComparer<List<LogDirectoryTree>>.Default.Equals(DirectoryTree, other.DirectoryTree);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(RootPath, DirectoryTree);
+    }
+}
+
+public class LogFileOption : IEquatable<LogFileOption?>
+{
     /// <summary>
     /// Unit : mb <br/><br/>
     /// Value : <br/>
@@ -48,15 +252,138 @@ public class LogOption
     /// </summary>
     public uint LogFileSize = 0;
 
-    public LogOption()
-    { }
+    public List<LogFileCreatingRule> FileCreatingRules = new();
 
-    public LogOption(Enum? logType, LogWriteOption logWriteOption, string rootPath, string logFilePattern, uint logFileSize)
+    public string CustomName = "";
+    public string Extension = ".log";
+
+    internal string GetFileName(string loggerName, DateTime logTime)
     {
-        LogType = logType;
-        LogWriteOption = logWriteOption;
-        RootPath = rootPath;
-        LogFilePattern = logFilePattern;
-        LogFileSize = logFileSize;
+        string name = string.Empty;
+        foreach (var rule in FileCreatingRules)
+        {
+            name += GetPath(rule);
+        }
+        return name + Extension;
+
+        string GetPath(LogFileCreatingRule rule)
+        {
+            return rule switch
+            {
+                LogFileCreatingRule.LoggerName => loggerName,
+                LogFileCreatingRule.CustomName => CustomName,
+                LogFileCreatingRule.DateTime_Second => $"{logTime:ss}",
+                LogFileCreatingRule.DateTime_Minute => $"{logTime:mm}",
+                LogFileCreatingRule.DateTime_Hour => $"{logTime:HH}",
+                LogFileCreatingRule.DateTime_Day => $"{logTime:dd}",
+                LogFileCreatingRule.DateTime_Month => $"{logTime:MM}",
+                LogFileCreatingRule.DateTime_Year => $"{logTime:yyyy}",
+                LogFileCreatingRule.Dot => ".",
+                LogFileCreatingRule.Space => " ",
+                LogFileCreatingRule.Underbar => "_",
+                LogFileCreatingRule.Dash => "-",
+                _ => throw new ArgumentException($"Not supported rule item : {rule}")
+            };
+        }
+    }
+
+    public static bool operator ==(LogFileOption? left, LogFileOption? right)
+    {
+        return EqualityComparer<LogFileOption>.Default.Equals(left, right);
+    }
+
+    public static bool operator !=(LogFileOption? left, LogFileOption? right)
+    {
+        return !(left == right);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as LogFileOption);
+    }
+
+    public bool Equals(LogFileOption? other)
+    {
+        return other is not null &&
+               LogFileSize == other.LogFileSize &&
+               EqualityComparer<List<LogFileCreatingRule>>.Default.Equals(FileCreatingRules, other.FileCreatingRules) &&
+               CustomName == other.CustomName &&
+               Extension == other.Extension;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(LogFileSize, FileCreatingRules, CustomName, Extension);
+    }
+}
+
+public class LogMessageOption : IEquatable<LogMessageOption?>
+{
+    public List<LogMessagePattern> MessagePatterns = new();
+
+    public LogMessageOption()
+    {
+        MessagePatterns = new()
+        {
+            LogMessagePattern.DateTime,
+            LogMessagePattern.LoggerName,
+            LogMessagePattern.Message,
+            LogMessagePattern.NewLine
+        };
+    }
+
+    public LogMessageOption(List<LogMessagePattern> patterns)
+    {
+        MessagePatterns = patterns.ToList();
+    }
+
+    internal string BuildMessage((DateTime DateTime, string Message, LogOption Option) contents)
+    {
+        if (MessagePatterns.Count == 0 || MessagePatterns.Contains(LogMessagePattern.None)) return contents.Message;
+        string rtnString = string.Empty;
+        foreach (var pattern in MessagePatterns)
+        {
+            rtnString += GetMessagePattern(pattern);
+        }
+        return rtnString;
+
+        string GetMessagePattern(LogMessagePattern patternItem)
+        {
+            return patternItem switch
+            {
+                LogMessagePattern.None => "",
+                LogMessagePattern.LoggerName => $"[{contents.Option.LoggerName}] ",
+                LogMessagePattern.DateTime => $"[{contents.DateTime:yyyy-MM-dd HH.mm.ss.fff}] ",
+                LogMessagePattern.Message => $"{contents.Message} ",
+                LogMessagePattern.NewLine => Environment.NewLine,
+                _ => throw new ArgumentException($"Not supported pattern item : {patternItem}")
+            };
+        }
+    }
+
+    public static bool operator ==(LogMessageOption? left, LogMessageOption? right)
+    {
+        return EqualityComparer<LogMessageOption>.Default.Equals(left, right);
+    }
+
+    public static bool operator !=(LogMessageOption? left, LogMessageOption? right)
+    {
+        return !(left == right);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as LogMessageOption);
+    }
+
+    public bool Equals(LogMessageOption? other)
+    {
+        return other is not null &&
+               EqualityComparer<List<LogMessagePattern>>.Default.Equals(MessagePatterns, other.MessagePatterns);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(MessagePatterns);
     }
 }
--- Peponi.Logger/Peponi.Logger/Log.cs
+++ Peponi.Logger/Peponi.Logger/Log.cs
@@ -1,102 +1,51 @@
 ﻿using Peponi.Logger.Processor;
-using Peponi.Logger.Writer;
-using Peponi.Utility.Helpers;
 
 namespace Peponi.Logger;
 
-/// <summary>
-/// Logger class. <br/><br/>
-/// Call <br/>
-/// 1. <see cref="Configure(LogOption)"/> or <br/>
-/// 2. <see cref="Configure(Enum, LogWriteOption, string?, string?, uint)"/> at startup.
-/// </summary>
-public static class Log
+public class Log
 {
-    private static bool _isConfigured = false;
+    private LogOption _option;
+    private LogProcessor _processor = new();
 
-    /// <summary>
-    /// Configure logger.
-    /// </summary>
-    /// <param name="option">Log option</param>
-    public static void Configure(LogOption option)
+    public Log(LogOption option)
     {
-        var logTypes = Enum.GetNames(option.LogType!.GetType()).ToList();
-        CheckLogPath(option.LogWriteOption, $@"{option.RootPath}\", logTypes);
-
-        LogProcessor.Configure(option.LogWriteOption, logTypes, $@"{option.RootPath}\", option.LogFilePattern);
-        LogWriter.Configure(option.LogWriteOption, logTypes, option.LogFileSize);
-
-        _isConfigured = true;
+        _option = option;
     }
 
-    /// <summary>
-    /// Configure logger.
-    /// </summary>
-    /// <param name="logType">Any enum type user defined</param>
-    /// <param name="writeOption">
-    /// 1. <see cref="LogWriteOption.OneFile"/><br/>
-    /// 2. <see cref="LogWriteOption.SeperateFile"/><br/>
-    /// 3. <see cref="LogWriteOption.SeperateFolder"/><br/>
-    /// </param>
-    /// <param name="rootPath">Root path of log</param>
-    /// <param name="logFilePattern">
-    /// Saving time unit of log.<br/>
-    /// Equal to DateTime.Now.ToString() format<br/><br/>
-    /// ex:<br/>
-    /// "yyyy-MM-dd"
-    /// </param>
-    /// <param name="logFileSize">
-    /// Unit : mb <br/><br/>
-    /// Value : <br/>
-    /// 0 = Inf <br/>
-    /// X = X mb <br/>
-    /// </param>
-    public static void Configure(Enum logType, LogWriteOption writeOption, string? rootPath = null, string? logFilePattern = null, uint logFileSize = 0)
+    public static Log GetLogger(LogOption? option = null)
     {
-        var logTypes = Enum.GetNames(logType.GetType()).ToList();
-
-        rootPath = $@"{rootPath}\" ?? $@"{Environment.CurrentDirectory}\Log\";
-        logFilePattern = logFilePattern ?? "yyyy-MM-dd";
-        CheckLogPath(writeOption, rootPath, logTypes);
-
-        LogProcessor.Configure(writeOption, logTypes, rootPath, logFilePattern);
-        LogWriter.Configure(writeOption, logTypes, logFileSize);
-
-        _isConfigured = true;
+        return option != null ? new Log(option) : new Log(GetDefaultOptions());
     }
 
-    /// <summary>
-    /// Write log.
-    /// </summary>
-    /// <param name="logType">Configrated log type</param>
-    /// <param name="message">Message want to log</param>
-    /// <exception cref="ArgumentNullException"></exception>
-    /// <exception cref="AccessViolationException"></exception>
-    public static void WriteLog(Enum logType, string message)
+    private static LogOption GetDefaultOptions()
     {
-        if (_isConfigured)
+        LogDirectoryOption dirOption = new LogDirectoryOption()
         {
-            // Exception will be raised case of null or unregistered log type
-            LogProcessor.WriteLog(DateTime.Now, logType.ToString()!, message);
-        }
-        else
+            DirectoryTree = new List<LogDirectoryTree>()
+                {
+                    LogDirectoryTree.None
+                }
+        };
+        LogFileOption fileOption = new LogFileOption()
         {
-            throw new AccessViolationException("Log is not configured");
-        }
+            LogFileSize = 100,
+            FileCreatingRules = new List<LogFileCreatingRule>()
+                {
+                    LogFileCreatingRule.DateTime_Year,
+                    LogFileCreatingRule.DateTime_Month,
+                    LogFileCreatingRule.DateTime_Day,
+                    LogFileCreatingRule.Underbar,
+                    LogFileCreatingRule.LoggerName
+                }
+        };
+        LogMessageOption msgOption = new LogMessageOption();
+
+        return new LogOption("Log", dirOption, fileOption, msgOption);
     }
 
-    private static void CheckLogPath(LogWriteOption writeOption, string rootPath, List<string> logTypes)
+    public void WriteLog(string message, DateTime? dateTime = null)
     {
-        DirectoryHelper.CreateDirectory(rootPath);
-
-        if (writeOption == LogWriteOption.SeperateFolder)
-        {
-            foreach (var logType in logTypes)
-            {
-                var totalLogPath = $@"{rootPath}\{logType}\";
-
-                DirectoryHelper.CreateDirectory(totalLogPath);
-            }
-        }
+        dateTime ??= DateTime.Now;
+        _processor.WriteLog(message, (DateTime)dateTime, _option);
     }
 }
